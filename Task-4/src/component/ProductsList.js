daa4a0733639c32d4b205300f31001ae6df941e0
HaMaDa <abdallahhamada2103@gmail.com>
2024-02-07 16:28:02 +0200

    last -v of task -4

--- Task-4/src/component/ProductsList.js
+++ Task-4/src/component/ProductsList.js
@@ -1,61 +1,36 @@
 import React from 'react';
 import "./ProductsList.css";
-import ProductsData from "../ProductsData"
 import OnlyProduct from './OnlyProduct';
-
 class ProductsList extends React.Component{
 
-state ={
-    i:"all"
-}
 
-//function which used in buttons
-change1=()=>{
-    this.setState({ 
+    constructor(props){
+        super(props);
+
+        this.state ={
+            i:"all",
+            ProductsData:[] // where i will wrap the data
+        }
+    }
 
 
-        i:"all"
-    })
-    
-}
-change2=()=>{
-    
-    this.setState({ 
+    componentDidMount(){
+        fetch('https://fakestoreapi.com/products/')
+        .then((response)=>response.json())
+        .then((data)=>{
+            this.setState({
+                ProductsData:data
+            })
+        });
+    }
 
-        i:"men's clothing"
-    })
-}
-change3=()=>{
-  
-    this.setState({ 
-
-        i:"jewelery"
-    })
-   
-}
-change4=()=>{
-    
-    this.setState({ 
-
-        i:"electronics"
-    })
-    
-}
-change5=()=>{
-    
-    this.setState({ 
-
-        i:"women's clothing"
-    })
-    
-}
 
 
     render(){
         
        let  btn=this.state.i
 
-        const products=ProductsData.map((product)=>{
+        const products=this.state.ProductsData.map((product)=>{
             
             return(
                 
@@ -72,11 +47,11 @@ change5=()=>{
         return(
             <>
                 <div className='buttons'>
-                    <button  className='all' onClick={this.change1}  >All</button>
-                    <button className='men' onClick={this.change2} >Men</button>
-                    <button  className='all' onClick={this.change3}  >Jewelery</button>
-                    <button className='electronics' onClick={this.change4} >Electronics</button>
-                    <button className='women'  onClick={this.change5}>Women</button>
+                    <button  className='all' onClick={()=>{this.setState({i:"all"})}}>All</button>
+                    <button className='men' onClick={()=>{this.setState({i:"men's clothing"})}}>Men</button>
+                    <button  className='all' onClick={()=>{this.setState({i:"jewelery"})}}>Jewelery</button>
+                    <button className='electronics' onClick={()=>{this.setState({i:"electronics"})}}>Electronics</button>
+                    <button className='women'  onClick={()=>{this.setState({i:"women's clothing"})}}>Women</button>
                 </div>
         
 
