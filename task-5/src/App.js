ecd352bf2d8aff507a253190632ecffaece49888
HaMaDa <abdallahhamada2103@gmail.com>
2024-02-16 00:50:07 +0200

    task-6

--- task-5/src/App.js
+++ task-5/src/App.js
@@ -11,16 +11,24 @@ const [advice,setadvice]=useState("");
 // }
 
 
-useEffect(()=>{
+ async function getData () {
+  let response= await fetch("https://api.adviceslip.com/advice");
+  let data= (await response.json())
+  setadvice(data.slip);
+ }
 
-    fetch("https://api.adviceslip.com/advice")
-    .then((response)=>response.json())
-    .then((data)=>{
-      setadvice(data.slip);
+ useEffect(()=>{
+  getData();
 
-  });
+  //    fetch("https://api.adviceslip.com/advice")
+  //   .then((response)=>response.json())
+  //   .then((data)=>{
+  //     setadvice(data.slip);
+
+  // });
 
 },[counter])
+
 
 
 
@@ -31,11 +39,9 @@ useEffect(()=>{
       <p className='text'>{advice.advice}</p>
       <img className='pattern' src='advice-generator-app-main/images/pattern-divider-desktop.svg'/>
 
-      <button className='hold flex ' onClick={()=>setCounter(counter + 1)}>
+      <button className='hold flex 'onClick={()=>setCounter(counter + 1)}>
       <img className='dice' src='advice-generator-app-main/images/icon-dice.svg'/>
       </button>
-
-  
 
     </div>
   );
